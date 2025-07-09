a488c6a9b64ec79acff16be0678d6236d5f12e4b
Abdullah Ahmad <abdullah.devloper@gmail.com>
2025-07-10 04:19:54 +0500

    update: src/config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://abdullah.api.digiservicezone.com/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
